18d46aaa0322707fea97f95a5d003c5cd57ec488
Enas4 <harleydavidson789@icloud.com>
2022-06-08 16:18:30 +0500

    changed css

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,9 @@ import "./Cards.css";
 const Cards = (props) => {
   return (
     <div className="Cards-card">
-      <img src={props.imgsrc} alt="" />
+      <div className="Card-img-container">
+        <img src={props.imgsrc} alt="" />
+      </div>
       <div className="Cards-card-info">
         <h2 className="Cards-card-title">{props.title}</h2>
         <a
